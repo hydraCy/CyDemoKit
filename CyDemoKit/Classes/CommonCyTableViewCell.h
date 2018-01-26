644da46bfec6c63ccc34b3a419fa02b93cb2344b
蔡宇 <caiyu@caiyudeMacBook-Pro.local>
2018-01-26 10:24:34 +0800

    no message

---
+++ CyDemoKit/Classes/CommonCyTableViewCell.h
@@ -0,0 +1,12 @@
+//
+//  CommonCyTableViewCell.h
+//  CyDemoKit
+//
+//  Created by 蔡宇 on 2018/1/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommonCyTableViewCell : UITableViewCell
+
+@end
---
+++ CyDemoKit/Classes/CommonCyTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  CommonCyTableViewCell.m
+//  CyDemoKit
+//
+//  Created by 蔡宇 on 2018/1/26.
+//
+
+#import "CommonCyTableViewCell.h"
+
+@implementation CommonCyTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
